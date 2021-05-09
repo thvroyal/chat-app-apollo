3e67409c0c4b765dd0dd60acb6fd5eb18dde6a3f
Hoang Viet <thvroyal@gmail.com>
2021-05-09 12:05:03 +0700

    part 2

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+const { UserInputError } = require("apollo-server");
 const { User } = require("../models");
 
 module.exports = {
@@ -9,6 +11,52 @@ module.exports = {
         return users;
       } catch (error) {
         console.log(error);
+      }
+    },
+  },
+  Mutation: {
+    register: async (_, args) => {
+      let { username, email, password, confirmPassword } = args;
+      let errors = {};
+      try {
+        // Validate input data
+        if (email.trim() === "") errors.email = "email must not be empty";
+        if (username.trim() === "")
+          errors.username = "username must not be empty";
+        if (password.trim() === "")
+          errors.password = "password must not be empty";
+        if (confirmPassword.trim() === "")
+          errors.confirmPassword = "confirm password must not be empty";
+
+        if (password !== confirmPassword)
+          errors.confirmPassword = "password must match";
+        //Check if username / email exists
+        // const userByUsername = await User.findOne({ where: { username } });
+        // const userByEmail = await User.findOne({ where: { email } });
+
+        // if (userByUsername) errors.username = "username is taken";
+        // if (userByEmail) errors.email = "email is taken";
+
+        if (Object.keys(errors).length > 0) {
+          throw errors;
+        }
+        //Hash password
+        password = await bcrypt.hash(password, 6);
+
+        //Create user
+        const user = await User.create({
+          username,
+          email,
+          password,
+        });
+        //return user
+        return user;
+      } catch (error) {
+        console.log(error);
+        if (error.name === "SequelizeUniqueConstraintError") {
+          error.errors.forEach((e) => (errors[e.path] = e.message));
+        }
+        throw new UserInputError("Bad input", { errors });
       }
     },
   },
